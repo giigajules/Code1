e4930f20de4ad11b4035395e095ee086bb32eef1
giigajules <giigajules@gmail.com>
2025-06-25 11:10:28 +0200

    f

---
+++ Code1VSCode/CoolCode.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello, World! You sucker.");
+let v = "1";
+v = v + 1;
+console.log(v);
---
+++ Code1VSCode/CoolCode.ts
@@ -0,0 +1,4 @@
+console.log("Hello, World! You sucker.");
+let v = "1";
+v = v + 1;
+console.log(v);
